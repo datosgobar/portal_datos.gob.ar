99505d9a085c49e106e9b24f014a584637ec08ec
Jose A. Salgado <jose.salgado.wrk@gmail.com>
2016-12-29 19:47:47 -0300

    Cambio tareas de inicializacion para el container de datos.gob.ar(web-app)

--- ckan-img/scripts/.make_conf.sh
+++ ckan-img/scripts/.make_conf.sh
@@ -12,19 +12,8 @@ abort () {
 }
 
 write_config () {
-  CKAN_IP=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-
   "$CKAN_HOME"/bin/paster --plugin=ckan config-tool "$CONFIG" -e \
-      "ckan.storage_path = ${CKAN_DATA}" \
-      "ckan.plugins = stats text_view image_view recline_view hierarchy_display hierarchy_form gobar_theme datastore datapusher"  \
-      "ckan.auth.create_user_via_api = false" \
-      "ckan.auth.create_user_via_web = false" \
-      "ckan.locale_default = es" \
-      "email_to = disabled@example.com" \
-      "ckan.datapusher.url = http://${CKAN_IP}:8800" \
-      "ckan.max_resource_size = 300" \
-      "error_email_from = ckan@$(hostname -f)" \
-      "ckan.site_url = http://${CKAN_IP}"
+      "error_email_from = ckan@$(hostname -f)" 
 
   if [ -n "$ERROR_EMAIL" ]; then
     sed -i -e "s&^#email_to.*&email_to = ${ERROR_EMAIL}&" "$CONFIG"
