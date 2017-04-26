b4fcfa203850deff570ca625c4afc5a3db517dce
Poli García <poli@devartis.com>
2017-04-25 23:26:09 -0300

    Intento de eliminar el error 127

--- deploy/update_testing.sh
+++ deploy/update_testing.sh
@@ -12,16 +12,5 @@ fi
 
 image_full_name="datosgobar/portal_datos.gob.ar:$tag"
 docker tag "$container_image" "$image_full_name"
-
-# Esta tarea podría tardar mucho, y según la documentación de Travis, las tareas que demoran más de 
-# 10 minutos generan un timeout (https://docs.travis-ci.com/user/customizing-the-build/#Build-Timeouts).
-# Para evitar este timeout forzamos un print cada 3 minutos indicando que el trabajo continua
-# Si docker tuviese un flag verbose quizás esto no sería necesario.
-docker push "$image_full_name" &
-
-export PID=$!
-while [[ `ps -p $PID | tail -n +2` ]]; do
-  echo 'Deploying'
-  sleep 540
-done
+docker push "$image_full_name"
 
