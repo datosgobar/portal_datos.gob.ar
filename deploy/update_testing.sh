582652cec2766a3bb1e9699307b20ea9a65d8026
Poli García <poli@devartis.com>
2017-04-26 00:06:49 -0300

    Testing deployment...

--- deploy/update_testing.sh
+++ deploy/update_testing.sh
@@ -2,15 +2,15 @@
 
 set -ev
 
-container_image=$(docker ps --format '{{ .Image }}' --filter 'name=portal_development')
+#container_image=$(docker ps --format '{{ .Image }}' --filter 'name=portal_development')
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-    tag="latest"
-else
-    tag="$TRAVIS_BRANCH"
-fi
+#if [ "$TRAVIS_BRANCH" == "master" ]; then
+#    tag="latest"
+#else
+#    tag="$TRAVIS_BRANCH"
+#fi
 
-image_full_name="datosgobar/portal_datos.gob.ar:$tag"
-docker tag "$container_image" "$image_full_name"
-docker push "$image_full_name"
+#image_full_name="datosgobar/portal_datos.gob.ar:$tag"
+#docker tag "$container_image" "$image_full_name"
+#docker push "$image_full_name"
 
