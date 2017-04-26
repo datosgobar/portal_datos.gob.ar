5e3d53afee93ac25f9276b54cb9c54bf6cc2d704
Poli García <poli@devartis.com>
2017-04-26 08:09:29 -0300

    Agrego algo de info de debug

--- deploy/update_testing.sh
+++ deploy/update_testing.sh
@@ -11,6 +11,10 @@ else
 fi
 
 image_full_name="datosgobar/portal_datos.gob.ar:$tag"
+
+echo "Container image: $container_image"
+echo "Image full name: $image_full_name"
+
 docker tag "$container_image" "$image_full_name"
 docker push "$image_full_name"
 
